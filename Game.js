7373f5272ea05f757a101e1554af2b92ed49911f
Jonphia Hart <soldiergurl4life@gmail.com>
2023-01-30 21:07:13 -0500

    At a stopping point with my game.js

--- Game.js
+++ Game.js
@@ -2,3 +2,26 @@ let winnerCount = 0;
 let loserCount = 0;
 
 let clickSound = document.querySelector("#click-sound");
+
+const img = document.querySelector("img");
+
+function logWin() {
+    document.getElementById("Result").innerHTML = "You Won!";
+    winnerCount += 1;
+}
+
+function logLoss() {
+    document.getElementById("Result").innerHTML = "You Lose!";
+    loserCount += 1;
+}
+
+function tieResult() {
+    document.getElementById("Result").innerHTML = "Tie! Try Again.";
+    document.getElementById("Result-Explained").innerHTML = "Draw";
+    img.src =
+    img.alt =
+}
+
+function logCounter() {
+    document.getElementById
+}
