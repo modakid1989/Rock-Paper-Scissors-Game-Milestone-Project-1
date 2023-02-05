c2afb2a18ec7d87d7456765be773401a4d849e93
Jonphia Hart <soldiergurl4life@gmail.com>
2023-02-05 09:55:59 -0500

    made minor changes to index.html

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ let roundWinner = ''
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        roundWinner = 'Tie'
+      roundWinner = 'Tie'
     }
-}
 
-if (
-    (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-    (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
-    (playerSelection === 'Paper' && computerSelection === 'Rock')
-) {
-    playerScore++
-    roundWinner = 'Player'
+
+if ( 
+(playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+(playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+(playerSelection === 'Paper' && computerSelection === 'Rock') 
+) 
+{
+playerScore++
+roundWinner = 'Player'
 }
 
 if (
@@ -68,8 +69,6 @@ if (isGameOver()) {
 openEndgameModal()
 return
 }
-      
-}
 const computerSelection = getRandomChoice()
 playRound(playerSelection, computerSelection)
 updateChoices(playerSelection, computerSelection)
@@ -79,6 +78,9 @@ if (isGameOver()) {
 openEndgameModal()
 setFinalMessage()
 }
+}
+      
+
 
 
 function updateChoices(playerSelection, computerSelection) {
@@ -86,6 +88,85 @@ switch (playerSelection) {
 case 'Rock':
 playerSign.textContent ='&#9994;&#127998;'
 break
+case 'Paper':
+playerSign.textContent ='&#9995;&#127998;'
+break
+case 'Scissors':
+playerSign.textContent ='&#9996;&#127998;'
+break
 }
 
+switch (computerSelection) {
+case 'Rock':
+computerSign.textContent ='&#9994;&#127998;'
+break
+case 'PAPER':
+      computerSign.textContent = '&#9995;&#127998;'
+      break
+    case 'SCISSORS':
+      computerSign.textContent = '&#9996;&#127998;'
+      break
 }
+}
+
+function updateScore() {
+if (roundWinner === 'tie') {
+scoreInfo.textContent = "It's a tie!"
+} else if (roundWinner === 'player') {
+scoreInfo.textContent = 'Winner,Winner!'
+} else if (roundWinner === 'computer') {
+scoreInfo.textContent = 'You lost! Try Again!'
+}
+playerScorePara.textContent = `Player: ${playerScore}`
+computerScorePara.textContent = `Computer: ${computerScore}`
+}
+function updateScoreMessage(winner, playerSelection, computerSelection) {
+    if (winner === 'player') {
+      scoreMessage.textContent = `${capitalizeFirstLetter(
+        playerSelection
+      )} beats ${computerSelection.toLowerCase()}`
+      return
+    }
+    if (winner === 'computer') {
+      scoreMessage.textContent = `${capitalizeFirstLetter(
+        playerSelection
+      )} is beaten by ${computerSelection.toLowerCase()}`
+      return
+    }
+  
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} ties with ${computerSelection.toLowerCase()}`
+  }
+  
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+  }
+  
+  function openEndgameModal() {
+    endgameModal.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function closeEndgameModal() {
+    endgameModal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
+  
+  function setFinalMessage() {
+    return playerScore > computerScore
+      ? (endgameMsg.textContent = 'You won!')
+      : (endgameMsg.textContent = 'You lost...')
+  }
+  
+  function restartGame() {
+    playerScore = 0
+    computerScore = 0
+    scoreInfo.textContent = 'Make A Selection'
+    scoreMessage.textContent = '1st to 5 is the winner'
+    playerScorePara.textContent = 'Player: 0'
+    computerScorePara.textContent = 'Computer: 0'
+    playerSign.textContent = '❔'
+    computerSign.textContent = '❔'
+    endgameModal.classList.remove('active')
+    overlay.classList.remove('active')
